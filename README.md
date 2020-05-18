c79e3fd1c132b38502b001ce1374bbbd03069471
Solomon Sklash <solomonsklash@0xfeed.io>
2020-05-17 23:46:37 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # RubeusToCcache
 A small tool to convert base64 encoded .kirbi tickets from Rubeus into .ccache files for Impacket
+
+## Overview
+
+This is a small tool to handle a common use case I run into often. Rubeus output TGTs in base64 encoded .kirbi files,
+whereas Impacket tools, like wmiexec.py or smbexec.py use the .ccache file format. The two formats are easily
+converted between, thanks to Impacket and Zer1t0's [ticket_converter.py](https://github.com/Zer1t0/ticket_converter).
+But I wanted to be able to use the Rubeus `/nowrap` option and send it right to a tool to get both .kirib and .ccache
+files. Which is what this script does. Big thanks to Zer1t0 and the Impacket project.
+
+## Usage
+
+```
+╦═╗┬ ┬┌┐ ┌─┐┬ ┬┌─┐  ┌┬┐┌─┐  ┌─┐┌─┐┌─┐┌─┐┬ ┬┌─┐
+╠╦╝│ │├┴┐├┤ │ │└─┐   │ │ │  │  │  ├─┤│  ├─┤├┤
+╩╚═└─┘└─┘└─┘└─┘└─┘   ┴ └─┘  └─┘└─┘┴ ┴└─┘┴ ┴└─┘
+              By Solomon Sklash
+          github.com/SolomonSklash
+   Inspired by Zer1t0's ticket_converter.py
+
+usage: rubeustoccache.py [-h] base64_input kirbi ccache
+
+positional arguments:
+  base64_input  The base64 encoded .kirbi, sucha as from Rubeus.
+  kirbi         The name of the output file for the decoded .kirbi file.
+  ccache        The name of the output file for the ccache file.
+
+optional arguments:
+  -h, --help    show this help message and exit
+```
+
+## Dependencies and Installation
+
+RubeusToCcache is written for Python 3, and requires the impacket and pyasn1 Python libraries.
+
+Run either `pip3 install impacket pyasn1` or `pip3 install -r requirements.txt`
+
+```
+git clone https://github.com/SolomonSklash/RubeusToCcache
+cd RubeusToCcache
+pip3 install -r requirements.txt
+```
---
+++ rubeustoccache.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+#
+# Author:
+#    Solomon Sklash (https://github.com/SolomonSklash)
+#    Heavily based on ticket_converter.py by Zer1t0 (https://github.com/Zer1t0)
+#
+# Description:
+#    This script will convert the base64 representation of .kirbi files into both a decoded
+#    .kirbi and a .ccache file, as used by Impacket scripts.
+#
+# References:
+#    https://github.com/Zer1t0/ticket_converter
+#    https://tools.ietf.org/html/rfc4120
+#    http://web.mit.edu/KERBEROS/krb5-devel/doc/formats/ccache_file_format.html
+#    https://github.com/gentilkiwi/kekeo
+#    https://github.com/rvazarkar/KrbCredExport
+#
+# Usage:
+#    ./rubeustoccache.py BASE64_BLOB ticket.kirbi ticket.ccache
+#
+import sys
+import base64
+import argparse
+from impacket.krb5.ccache import CCache
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('base64_input', help="The base64 encoded .kirbi, sucha as from Rubeus.")
+    parser.add_argument('kirbi', help="The name of the output file for the decoded .kirbi file.")
+    parser.add_argument('ccache', help="The name of the output file for the ccache file.")
+    return parser.parse_args()
+
+def main():
+    banner =  "╦═╗┬ ┬┌┐ ┌─┐┬ ┬┌─┐  ┌┬┐┌─┐  ┌─┐┌─┐┌─┐┌─┐┬ ┬┌─┐\n"
+    banner += "╠╦╝│ │├┴┐├┤ │ │└─┐   │ │ │  │  │  ├─┤│  ├─┤├┤ \n"
+    banner += "╩╚═└─┘└─┘└─┘└─┘└─┘   ┴ └─┘  └─┘└─┘┴ ┴└─┘┴ ┴└─┘\n"
+    banner += (" " * 14) + "By Solomon Sklash\n"
+    banner += (" " * 10) + "github.com/SolomonSklash\n"
+    banner += (" " * 2) + " Inspired by Zer1t0's ticket_converter.py\n"
+
+    print(banner)
+
+    args = parse_args()
+
+    try:
+        result = base64.b64decode(args.base64_input.encode('ascii'))
+    except:
+        print("[!] Failed to decode provided base64 string.")
+        exit(1)
+    try:
+        with open(args.kirbi, 'wb') as kirbi_file:
+            kirbi_file.write(result)
+            kirbi_file.close()
+            print("[*] Writing decoded .kirbi file to " + str(args.kirbi))
+    except:
+        print("[!] Failed to write ticket to temporary .kirbi file.")
+
+    try:
+        convert_kirbi_to_ccache(args.kirbi, args.ccache)
+        print("[*] Writing converted .ccache file to " + str(args.ccache))
+    except:
+        print("[!] Failed to convert .kirbi file to .ccache file.")
+    print("[*] All done! Don't forget to set your environment variable: export KRB5CCNAME=" + str(args.ccache))
+
+def convert_kirbi_to_ccache(input_filename, output_filename):
+    ccache = CCache.loadKirbiFile(input_filename)
+    ccache.saveFile(output_filename)
+
+def convert_ccache_to_kirbi(input_filename, output_filename):
+    ccache = CCache.loadFile(input_filename)
+    ccache.saveKirbiFile(output_filename)
+
+if __name__ == '__main__':
+    main()
